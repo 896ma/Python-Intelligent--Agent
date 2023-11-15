2a33831fd3d8a949e0a23de13df0d349087df069
G_oaT <solarcrash3@gmail.com>
2023-11-15 20:40:50 +0300

    parse

--- main.py
+++ main.py
@@ -12,6 +12,10 @@ voices= engine.getProperty('voices')
 engine.setProperty('voice,voices[0].id') #0 -male ,1- female
 activationWord ='computer' #single word
 
+#Method to have the system listening for commands
 
+def parsecommand():
+    listener = sr.Recognizer()
+    print('Listening for  a user command :)') 
 
 
