8c243ae699da42b86d1f49da54ac637dceba3ace
G_oaT <solarcrash3@gmail.com>
2023-11-15 20:48:41 +0300

    exeception handling

--- main.py
+++ main.py
@@ -17,5 +17,23 @@ activationWord ='computer' #single word
 def parsecommand():
     listener = sr.Recognizer()
     print('Listening for  a user command :)') 
+    
+    with sr.Microphone() as source:
+        listener.pause_threshold  =2 # how  long can there be a gap in your speech listening before it's cut
+        input_speech =listener.listen(source)
+        
+        
+    try:
+        print('Recognizing Speech...')
+        query =listener.recognize_google(input_speech,language='en_us')
+        print(f'The input speech was:(query)')#How well was the speech recognized
+    except Exception as exception:
+        print('Sorry but I did not quite get that :)')
+        
+        
+        
+        
+        
+    
 
 
