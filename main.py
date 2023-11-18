1bf72d412459f2eb975a46ac15a04a9141d254be
G_oaT <solarcrash3@gmail.com>
2023-11-18 11:36:40 +0300

    project  updated

--- main.py
+++ main.py
@@ -1,49 +1,134 @@
-from  datetime import datetime
-import speech_recognition  as sr   
+from datetime import datetime
+import speech_recognition as sr
 import pyttsx3
 import webbrowser
 import wikipedia
 import wolframalpha
+import keyboard
 import math
-import  time
-#speech engine initialisation
+import openai
 
-engine=pyttsx3.init()
-voices= engine.getProperty('voices')
-engine.setProperty('voice', voices[0].id)
- #0 -male ,1- female
-activationWord ='Patek' #single word
+# Load credentials
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
-#Browser configuration
-#set the path for the web browser 
-
+# Google TTS
+import google.cloud.texttospeech as tts
+import pygame
+import time
 
 # Mute ALSA errors...
-from ctypes import *
-from contextlib import contextmanager
+#from ctypes import *
+#from contextlib import contextmanager
 
-ERROR_HANDLER_FUNC = CFUNCTYPE(None, c_char_p, c_int, c_char_p, c_int, c_char_p)
+#ERROR_HANDLER_FUNC = CFUNCTYPE(None, c_char_p, c_int, c_char_p, c_int, c_char_p)
 
-def py_error_handler(filename, line, function, err, fmt):
-    pass
+#def py_error_handler(filename, line, function, err, fmt):
+ #   pass
 
-c_error_handler = ERROR_HANDLER_FUNC(py_error_handler)
+#c_error_handler = ERROR_HANDLER_FUNC(py_error_handler)
 
-@contextmanager
-def noalsaerr():
-    try: 
-        asound = cdll.LoadLibrary('libasound.so')
-        asound.snd_lib_error_set_handler(c_error_handler)
-        yield
-        asound.snd_lib_error_set_handler(None)
-    except:
-        yield
-        print('')
-chrome_path = "/usr/bin/google-chrome"
+#@contextmanager
+#def noalsaerr():
+   # try: 
+        #asound = cdll.LoadLibrary('libasound.so')
+        #asound.snd_lib_error_set_handler(c_error_handler)
+      #  yield
+        #asound.snd_lib_error_set_handler(None)
+ #   except:
+       # yield
+   #     print('')
 
-webbrowser.register('chrome', None, webbrowser.BackgroundBrowser(chrome_path))
+### PARAMETERS ###
+activationWords = ['Assistant', 'calcutron', 'shodan', 'showdown']
+tts_type = 'local' # google or local
+
+# Local speech engine initialisation
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[0].id) # 0 = male, 1 = female
+
+# Google TTS client
+def google_text_to_wav(voice_name: str, text: str):
+    language_code = "-".join(voice_name.split("-")[:2])
+
+    # Set the text input to be synthesized
+    text_input = tts.SynthesisInput(text=text)
+
+    # Build the voice request, select the language code ("en-US") and the voice name
+    voice_params = tts.VoiceSelectionParams(
+        language_code=language_code, name=voice_name
+    )
+
+    # Select the type of audio file you want returned
+    audio_config = tts.AudioConfig(audio_encoding=tts.AudioEncoding.LINEAR16)
+
+    client = tts.TextToSpeechClient()
+    response = client.synthesize_speech(
+        input=text_input, voice=voice_params, audio_config=audio_config
+    )
+
+    return response.audio_content
+
+# Configure browser
+# Set the path
+firefox_path = "usr/bin/firefox"
+# Register the browser
+webbrowser.register('firefox', None, 
+                    webbrowser.BackgroundBrowser(firefox_path))
+
+# Wolfram Alpha client
+appId = '5R49J7-J888YX9J2V'
+wolframClient = wolframalpha.Client(appId)
+
+def speak(text, rate = 120):
+    time.sleep(0.3)
+    try:     
+        if tts_type == 'local':
+            engine.setProperty('rate', rate) 
+            engine.say(text, 'txt')
+            engine.runAndWait()
+        if tts_type == 'google':
+            speech = google_text_to_wav('en-US-News-K', text)
+            pygame.mixer.init(frequency=12000, buffer = 512)
+            speech_sound = pygame.mixer.Sound(speech)
+            speech_length = int(math.ceil(pygame.mixer.Sound.get_length(speech_sound)))
+            speech_sound.play()
+            time.sleep(speech_length)
+            pygame.mixer.quit()
+ 
+    ## The standard keyboard interrupt is Ctrl+C. This interrupts the Google speech synthesis.
+    except KeyboardInterrupt:
+        try:
+            if tts_type == 'google':
+                pygame.mixer.quit()
+        except:
+            pass
+        return
 
 
+def parseCommand():
+   # with noalsaerr():
+        listener = sr.Recognizer()
+     #   print('Listening for a command')
+
+        with sr.Microphone() as source:
+            listener.pause_threshold = 2
+            input_speech = listener.listen(source)
+
+        try:
+            print('Recognizing speech...')
+            query = listener.recognize_google(input_speech, language='en_US')
+            print(f'The input speech was: {query}')
+
+        except Exception as exception:
+            print('Sorry I did not quite catch that')
+            print(exception)
+
+            return 'None'
+
+        return query
 
 def search_wikipedia(keyword=''):
     searchResults = wikipedia.search(keyword)
@@ -57,72 +142,122 @@ def search_wikipedia(keyword=''):
     wikiSummary = str(wikiPage.summary)
     return wikiSummary
 
-def speak(text,rate =190):
-    engine.setProperty('rate',rate)
-    engine.say(text)
-    engine.runAndWait()
-    
+def listOrDict(var):
+    if isinstance(var, list):
+        return var[0]['plaintext']
+    else:
+        return var['plaintext']
 
-#Method to have the system listening for commands
+def search_wolframalpha(keyword=''):
+    response = wolframClient.query(keyword)
+  
+    # @success: Wolfram Alpha was able to resolve the query
+    # @numpods: Number of results returned
+    # pod: List of results. This can also contain subpods
 
-def parsecommand():
-    listener = sr.Recognizer()
-    print('Listening for  a user command :)') 
-    
-    with sr.Microphone() as source:
-        listener.pause_threshold  =5 # how  long can there be a gap in your speech listening before it's cut
-        input_speech =listener.listen(source)
-        
-        
-    try:
-        print('Recognizing Speech...')
-        query =listener.recognize_google(input_speech,language='en_US')
-        print(f'The input speech was:(query)')#How well was the speech recognized
-    except Exception as exception:
-        print('Sorry but I did not quite catch that :)')
-        speak('Sorry but I did not quite catch that')
-        print(exception)
-        return 'None'
-    return query
+    # Query not resolved
+    if response['@success'] == 'false':
+        speak('I am sorry but  I  was unable to  compute that ')
+    # Query resolved
+    else: 
+        result = ''
+        # Question
+        pod0 = response['pod'][0]
+        # May contain answer (Has highest confidence value) 
+        # if it's primary or has the title of result or definition, then it's the official result
+        pod1 = response['pod'][1]
+        if (('result') in pod1['@title'].lower()) or (pod1.get('@primary', 'false') == 'true') or ('definition' in pod1['@title'].lower()):
+            # Get the result
+            result = listOrDict(pod1['subpod'])
+            # Remove bracketed section
+            return result.split('(')[0]
+        else:
+            # Get the interpretation from pod0
+            question = listOrDict(pod0['subpod'])
+            # Remove bracketed section
+            question = question.split('(')[0]
+            # Could search wiki instead here? 
+            return question
 
-#main loop
-if __name__ == '__main__':
-    speak('All  systems up and  running .')
-    
+def query_openai(prompt = ""):
+    openai.organization = os.environ['OPENAI_ORG']
+    openai.api_key = os.environ['OPENAI_API_KEY']
+
+    # Temperature is a measure of randonmess
+    # Max_tokens is the number of tokens to generate
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=prompt,
+        temperature=0.3,
+        max_tokens=80,
+
+    )
+
+    return response.choices[0].text
+
+# Main loop
+if __name__ == '__main__': 
+    speak('The  system is  up and running.', 120)
+
     while True:
-        #parse  arguments as a list( store whatever user says as a list of different words)
-        query =parsecommand().lower().split()
-        if query[0] == activationWord:
+        # Parse as a list
+        # query = 'computer say hello'.split()
+        query = parseCommand().lower().split()
+
+        if query[0] in activationWords and len(query) > 1:
             query.pop(0)
-        
-        #list commands
-        if  query[0] == 'say':
-            if 'hello' in query:
-                speak('Greetings , Solar Crash')
-                
-            else:
-                query.pop(0) #  Remove the  say command again from your list
-                speech =''.join(query)
-                speak(speech) #  Echo from your  AI assistant 
-                
-                #Navigation
-                
-            if query[0]== 'go' and query[1]== 'to':
-                speak('opening...')
-                query = ''.join(query[2:])
+
+            # Set commands
+            if query[0] == 'say':
+                if 'hello' in query:
+                    speak('Greetings, all!')
+                else:
+                    query.pop(0) # Remove 'say'
+                    speech = ' '.join(query) 
+                    speak(speech)
+
+            # Query OpenAI
+            if query[0] == 'insight':
+                query.pop(0) # Remove 'insight'
+                query = ' '.join(query)
+                speech = query_openai(query)
+                speak("Ok")
+                speak(speech)
+
+            # Navigation
+            if query[0] == 'go' and query[1] == 'to':
+                speak('Opening... ')
+                # Assume the structure is activation word + go to, so let's remove the next two words
+                query = ' '.join(query[2:])
                 webbrowser.get('chrome').open_new(query)
-                
-             # Wikipedia
+  
+            # Wikipedia
             if query[0] == 'wikipedia':
                 query = ' '.join(query[1:])
                 speak('Querying the universal databank')
                 time.sleep(2)
                 speak(search_wikipedia(query))
-        
-        
-    
-    
-        
-        
-        
-             
+
+            # Wolfram Alpha
+            if query[0] == 'compute' or query[0] == 'computer':
+                query = ' '.join(query[1:])
+                try:
+                    result = search_wolframalpha(query)
+                    speak(result)
+                except:
+                    speak('Unable to compute')
+
+            # Note taking
+            if query[0] == 'log':
+                speak('Ready to record your note')
+                newNote = parseCommand().lower()
+                now = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+                with open('note_%s.txt' % now, 'w') as newFile:
+                    newFile.write(now)
+                    newFile.write(' ')
+                    newFile.write(newNote)
+                speak('Note written')
+
+            if query[0] == 'exit':
+                speak('Goodbye')
+                break
